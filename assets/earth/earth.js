4427b0b7dcc3a7270e7877c2824b9e5ed90da9bd
Arslan51214 <arslannaveed5364337@gmail.com>
2025-01-16 21:10:33 +0500

    push init

--- assets/earth/earth.js
+++ assets/earth/earth.js
@@ -1,64 +1,51 @@
-/**
- * ---------------------------------------
- * This demo was created using amCharts 4.
- * 
- * For more information visit:
- * https://www.amcharts.com/
- * 
- * Documentation is available at:
- * https://www.amcharts.com/docs/v4/
- * ---------------------------------------
- */
+window.onload = function () {
+  const earthAnimation = document.getElementById("earth-animation");
+  const chartDiv = document.getElementById("chartdiv");
 
-// Themes begin
-am4core.useTheme(am4themes_animated);
-// Themes end
+  // Make the animation container visible with a fade-in effect
+  earthAnimation.style.display = "block";
+  setTimeout(() => {
+    chartDiv.style.opacity = "1";
+  }, 100);
 
-var chart = am4core.create("chartdiv", am4maps.MapChart);
+  // Initialize the amCharts Earth animation
+  am4core.useTheme(am4themes_animated);
 
-// Set map definition
-chart.geodata = am4geodata_worldLow;
+  var chart = am4core.create("chartdiv", am4maps.MapChart);
+  chart.geodata = am4geodata_worldLow;
+  chart.projection = new am4maps.projections.Orthographic();
+  chart.panBehavior = "rotateLongLat";
+  chart.deltaLatitude = -20;
+  chart.padding(20, 20, 20, 20);
 
-// Set projection
-chart.projection = new am4maps.projections.Orthographic();
-chart.panBehavior = "rotateLongLat";
-chart.deltaLatitude = -20;
-chart.padding(20,20,20,20);
+  var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+  polygonSeries.useGeodata = true;
 
-// Create map polygon series
-var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+  var polygonTemplate = polygonSeries.mapPolygons.template;
+  polygonTemplate.tooltipText = "{name}";
+  polygonTemplate.fill = am4core.color("#FF6633");
+  polygonTemplate.stroke = am4core.color("#000033");
+  polygonTemplate.strokeWidth = 0.5;
+  polygonTemplate.cursorOverStyle = am4core.MouseCursorStyle.pointer;
+  polygonTemplate.urlTarget = "_blank";
 
-// Make map load polygon (like country names) data from GeoJSON
-polygonSeries.useGeodata = true;
-//polygonSeries.include = ["BR", "UA", "MX", "CI"];
+  var graticuleSeries = chart.series.push(new am4maps.GraticuleSeries());
+  graticuleSeries.mapLines.template.line.stroke = am4core.color("#ffffff");
+  graticuleSeries.mapLines.template.line.strokeOpacity = 0.08;
+  graticuleSeries.fitExtent = false;
 
-// Configure series
-var polygonTemplate = polygonSeries.mapPolygons.template;
-polygonTemplate.tooltipText = "{name}";
-polygonTemplate.fill = am4core.color("#FF6633");
-polygonTemplate.stroke = am4core.color("#000033");
-polygonTemplate.strokeWidth = 0.5;
-polygonTemplate.cursorOverStyle = am4core.MouseCursorStyle.pointer;
-polygonTemplate.urlTarget = "_blank";
+  chart.backgroundSeries.mapPolygons.template.polygon.fillOpacity = 0.1;
+  chart.backgroundSeries.mapPolygons.template.polygon.fill = am4core.color("#ffffff");
 
-var graticuleSeries = chart.series.push(new am4maps.GraticuleSeries());
-graticuleSeries.mapLines.template.line.stroke = am4core.color("#ffffff");
-graticuleSeries.mapLines.template.line.strokeOpacity = 0.08;
-graticuleSeries.fitExtent = false;
+  var hs = polygonTemplate.states.create("hover");
+  hs.properties.fill = chart.colors.getIndex(0).brighten(-0.5);
 
+  let animation;
+  setTimeout(function () {
+    animation = chart.animate({ property: "deltaLongitude", to: 100000 }, 20000000);
+  }, 3000);
 
-chart.backgroundSeries.mapPolygons.template.polygon.fillOpacity = 0.1;
-chart.backgroundSeries.mapPolygons.template.polygon.fill = am4core.color("#ffffff");
-
-// Create hover state and set alternative fill color
-var hs = polygonTemplate.states.create("hover");
-hs.properties.fill = chart.colors.getIndex(0).brighten(-0.5);
-
-let animation;
-setTimeout(function(){
-  animation = chart.animate({property:"deltaLongitude", to:100000}, 20000000);
-}, 3000)
-
-chart.seriesContainer.events.on("down", function(){
-//  animation.stop();
-})
+  chart.seriesContainer.events.on("down", function () {
+    // animation.stop();
+  });
+};
